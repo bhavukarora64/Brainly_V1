86ef1eb71c15d7895553fb743249001838359a86
Bhavuk Arora <bhavuk.arora@rib-software.com>
2025-03-14 00:57:45 +0100

    Update CORS configuration to use environment variable for frontend origin and enable credentials

--- brainly_backend/src/index.ts
+++ brainly_backend/src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_BASE_URL, credentials: true }));
 app.options('*', cors());
 
 app.get("/", (req, res) => {
