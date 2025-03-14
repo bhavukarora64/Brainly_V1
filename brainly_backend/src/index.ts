5c2a4bcded584baf7e68c9e7f3fccb97608a09c8
Bhavuk Arora <bhavuk.arora@rib-software.com>
2025-03-14 01:07:36 +0100

    Update CORS configuration to use environment variable for frontend origin

--- brainly_backend/src/index.ts
+++ brainly_backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://brainly-v1-frontend.vercel.app',
+    origin: process.env.FRONTEND_BASE_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
